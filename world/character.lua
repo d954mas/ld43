1939d4c09fe3dcd35384df02c7496aaae2adcc67
d954mas <d954mas@mail.ru>
2018-12-01 17:41:16 +0300

    model

--- world/character.lua
+++ world/character.lua
@@ -1,32 +1,60 @@
 local COMMON = require "libs.common"
 local Observable = require "libs.observable_mixin"
+local ComplexSubscription = require "libs.complex_subscription"
 
 
 ---@class Character:Observable
 local M = COMMON.class("Character")
 M:include(Observable)
 local EVENTS = {
-    PLAY_ANIMATION = "PLAY_ANIMATION"
+    PLAY_ANIMATION = "PLAY_ANIMATION",
+    STATE_CHANGED = "STATE_CHANGED"
 }
 local STATES = {
     STOP = "STOP",
     WALKING = "WALKING"
 }
 
-function M:initialize(art)
+---@param world World
+function M:initialize(art, world)
     self.EVENTS = EVENTS
     self.STATES = STATES
+    self.world = world
     self:set_observable_events(self.EVENTS)
     self.art = assert(art)
     self.state = STATES.STOP
+    self.subscription = ComplexSubscription()
+    self.subscription:add(self.world:register_observer(self.world.EVENTS.STATE_CHANGED, function()
+        local state = self.world.state
+        if state == self.world.STATES.EVENT then
+            self:set_state(STATES.STOP)
+        elseif state == self.world.STATES.WALK then
+            self:set_state(STATES.WALKING)
+        end
+    end))
+end
+
+function M:set_state(state)
+    assert(STATES[state])
+    COMMON.LOG.w("state changed from:" .. self.state .. " " .. state)
+    if self.state ~= state then
+        self.state = state
+        self:observable_notify(self.EVENTS.STATE_CHANGED)
+    end
+
 end
 
 function M:play_animation()
 
+    self:observable_notify(self.EVENTS.PLAY_ANIMATION)
 end
 
 --update animation or other actions if needed
 function M:update(dt, no_save)
+end
+
+function M:dispose()
+    self.subscription:unsubscribe()
 end
 
 
--- world/world.lua
+++ world/world.lua
@@ -1,26 +1,48 @@
 local COMMON = require "libs.common"
 local Observable = require "libs.observable_mixin"
 local Character = require "world.character"
+local TAG = "World"
 
 ---@class World:Observable
 local M = COMMON.class("World")
 M:include(Observable)
 local EVENTS = {
-	GOLD_CHANGED = "GOLD_CHANGED",
-	SPAWN_ENEMY = "SPAWN_ENEMY",
-	ENEMY_DEAD = "ENEMY_DEAD"
+	STATE_CHANGED = "STATE_CHANGED",
+}
+
+local STATES = {
+	WALK = "WALK",
+	EVENT = "EVENT"
 }
 
 function M:initialize()
 	self.EVENTS = EVENTS
+	self.STATES = STATES
+	self.state = STATES.WALK
 	self:set_observable_events(self.EVENTS)
 	self.movement_speed = 100
 	self.time = 24 * 60 --minutes
-	self.characters = {Character("char_1"),
-	Character("char_2"),
-	Character("char_3"),
-	Character("char_4"),
-	Character("char_5")}
+	self.characters = {Character("char_1",self),
+	Character("char_2",self),
+	Character("char_3",self),
+	Character("char_4",self),
+	Character("char_5",self)}
+end
+
+function M:set_state(state)
+	assert(STATES[state])
+	if self.state ~= state then
+		COMMON.LOG.w("state changed from:" ..  self.state .. " to " .. state )
+		self.state = state
+
+		if self.state == STATES.EVENT then
+			self.movement_speed = 0
+		elseif self.state == STATES.WALK then
+			self.movement_speed = 50
+		end
+
+		self:observable_notify(EVENTS.STATE_CHANGED)
+	end
 end
 
 function M:update(dt, no_save)
