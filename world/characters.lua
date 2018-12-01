6cc80b38f3d9717916c068672961ff0080960447
d954mas <d954mas@mail.ru>
2018-12-01 19:01:20 +0300

    characters model

--- world/characters.lua
+++ world/characters.lua
@@ -1,19 +1,29 @@
 local COMMON = require "libs.common"
 
 local Char = COMMON.class("CharacterData")
-function Char:initialize(art)
-    self.art = assert(art)
+function Char:initialize(data)
+    self.art = assert(data.art)
+    self.name = data.name or ""
+    self.descr = data.descr or ""
+
+    for k,v in pairs(data) do
+        self[k] = v
+    end
+    --events params
+
+    --animations()
+    --
 end
 
 
 ---@type CharacterData[]
 local M = {}
 
-M[1] = Char("char_1")
-M[2] = Char("char_2")
-M[3] = Char("char_3")
-M[4] = Char("char_4")
-M[5] = Char("char_5")
+M[1] = Char{art = "char_1"}
+M[2] = Char{art = "char_2"}
+M[3] = Char{art = "char_3"}
+M[4] = Char{art = "char_4"}
+M[5] = Char{art = "char_5"}
 
 
 return M
