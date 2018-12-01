212368d5e86d247c227f5321dfd7941b7e63e060
d954mas <d954mas@mail.ru>
2018-12-01 18:54:42 +0300

    character click function

--- world/characters.lua
+++ world/characters.lua
@@ -1,16 +1,19 @@
----@class CharacterData
----@field art string
-local tmp = {}
+local COMMON = require "libs.common"
+
+local Char = COMMON.class("CharacterData")
+function Char:initialize(art)
+    self.art = assert(art)
+end
 
 
 ---@type CharacterData[]
 local M = {}
 
-M[1] = {art = "char_1"}
-M[2] = {art = "char_2"}
-M[3] = {art = "char_3"}
-M[4] = {art = "char_4"}
-M[5] = {art = "char_5"}
+M[1] = Char("char_1")
+M[2] = Char("char_2")
+M[3] = Char("char_3")
+M[4] = Char("char_4")
+M[5] = Char("char_5")
 
 
 return M
