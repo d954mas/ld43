86a16030af70a993877822cdb8e68a074e417a43
Alex [Byton] <alex.tyk@yandex.ru>
2018-12-01 18:46:32 +0300

    Fix global functions to local

--- scenes/game/event_manager/events/event_1.lua
+++ scenes/game/event_manager/events/event_1.lua
@@ -1,5 +1,16 @@
 local e = {
-
+  icon = "1",
+  text = "first event",
+  buttons = {
+    {
+      text = "temp",
+      action = "first_button"
+    },
+    {
+      text = "temp_2",
+      action = "second_button"
+    }
+  }
 }
 
 return e
