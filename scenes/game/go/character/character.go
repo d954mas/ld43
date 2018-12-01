1036a8b56edcae001aee9d5ab83cac59f126f1f3
d954mas <d954mas@mail.ru>
2018-12-01 20:10:31 +0300

    add lights

--- scenes/game/go/character/character.go
+++ scenes/game/go/character/character.go
@@ -33,3 +33,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/simple_lights/assets/lights.atlas\"\n"
+  "default_animation: \"light_mask\"\n"
+  "material: \"/simple_lights/materials/light.material\"\n"
+  "blend_mode: BLEND_MODE_ADD\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
