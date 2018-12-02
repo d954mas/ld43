6bd3fccf42cfc82a80915a7d041a5560d9116961
Alex [Byton] <alex.tyk@yandex.ru>
2018-12-02 16:00:51 +0300

    Update characters info gui

--- world/characters.lua
+++ world/characters.lua
@@ -20,11 +20,11 @@ end
 ---@type CharacterData[]
 local M = {}
 
-M[1] = Char{art = "char_1", name = "Ангелина", id = "angel"}
-M[2] = Char{art = "char_2", name = "Джон", id = "jon"}
-M[3] = Char{art = "char_3", name = "Карл", id = "carl"}
-M[4] = Char{art = "char_4", name = "Анабель", id = "anabel"}
-M[5] = Char{art = "char_5", name = "Роберт", id = "robert"}
+M[1] = Char{art = "char_1", name = "Ангелина", id = "angel", descr = "Просто Астронавт.", positive = {"Спортивная", "Умная"}, negative = {"Арахнофоб", "Самолюбивая"}}
+M[2] = Char{art = "char_2", name = "Джон", id = "jon", descr = "Просто боксер.", positive = {"Силач", "Красноречив"}, negative = {"Боязнь высоты", "Карманник"}}
+M[3] = Char{art = "char_3", name = "Карл", id = "carl", descr = "Просто художник.", positive = {"Творческий", "Отличный слух"}, negative = {"Слепота", "Обильная доверчивость"}}
+M[4] = Char{art = "char_4", name = "Анабель", id = "anabel", descr = "Просто медсестра.", positive = {"Знание медицины", "Навыки вышивания"}, negative = {"Нервная", "Курит"}}
+M[5] = Char{art = "char_5", name = "Роберт", id = "robert", descr = "Просто игрок в покер.", positive = {"Ловкий", "Расчетливый"}, negative = {"Недружелюбный", "Обманщик"}}
 
 
 return M
